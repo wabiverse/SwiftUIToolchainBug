c8a1648561e483aaee9a8176680a7b630677fc80
furby™ <devs@wabi.foundation>
2024-03-20 00:15:46 -0500

    Test with simpler algorithms dependency.

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 #if os(macOS)
   let swiftUIToolChainBug: [String: String]? = nil
 #else /* os(macOS) */
-  let swiftUIToolChainBug: [String: String]? = ["SwiftCrossUI": "SwiftUI"]
+  let swiftUIToolChainBug: [String: String]? = ["Algorithms": "SwiftUI"]
 #endif /* !os(macOS) */
 
 let package = Package(
@@ -35,7 +35,7 @@ let package = Package(
   ],
   dependencies: [
     // Dependencies declare other packages that this package depends on.
-    .package(url: "https://github.com/furby-tm/swift-cross-ui", revision: "a26353e")
+    .package(url: "https://github.com/apple/swift-algorithms", branch: "main")
   ],
   targets: [
     // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -47,14 +47,9 @@ let package = Package(
       dependencies: [
         .target(name: "CxxModule"),
         .product(
-          name: "SwiftCrossUI",
-          package: "swift-cross-ui",
+          name: "Algorithms",
+          package: "swift-algorithms",
           moduleAliases: swiftUIToolChainBug,
-          condition: .when(platforms: [.linux, .windows])
-        ),
-        .product(
-          name: "GtkBackend",
-          package: "swift-cross-ui",
           condition: .when(platforms: [.linux, .windows])
         ),
       ],
